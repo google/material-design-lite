f71a54cc82d66dcaf076f98cceb6da203dd3d7ed
Valentin Klinghammer <valentin@quelltextfabrik.de>
2014-07-03 16:36:41 +0200

    minifyHtml should only run on html files

--- gulpfile.js
+++ gulpfile.js
@@ -123,7 +123,7 @@ gulp.task('html', function () {
     // Update Production Style Guide Paths
     .pipe($.replace('components/components.css', 'components/main.min.css'))
     // Minify Any HTML
-    .pipe($.minifyHtml())
+    .pipe($.if('*.html', $.minifyHtml()))
     // Output Files
     .pipe(gulp.dest('dist'))
     .pipe($.size({title: 'html'}));
