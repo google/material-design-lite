6268654b544437111cb2083f8bb6aba9773d9ed4
Sindre Sorhus <sindresorhus@gmail.com>
2014-07-04 15:54:20 +0200

    followup to 4730569239d811666227aa681fc976d2a0ff6d0a

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -22,7 +22,7 @@
   var querySelector = document.querySelector.bind(document);
 
   var navdrawerContainer = querySelector('.navdrawer-container');
-  var body = querySelector('document.body');
+  var body = document.body;
   var appbarElement = querySelector('.app-bar');
   var menuBtn = querySelector('.menu');
   var main = querySelector('main');
