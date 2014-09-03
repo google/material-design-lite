5dfc2953062de29c4a3eb9312a900c9e7769b20d
Matt Gaunt <matt@gauntface.co.uk>
2014-09-03 12:04:46 +0100

    Adding watch to the styles for styleguide

--- gulpfile.js
+++ gulpfile.js
@@ -152,7 +152,7 @@ gulp.task('serve', ['styles'], function () {
   });
 
   gulp.watch(['app/**/*.html'], reload);
-  gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
+  gulp.watch(['app/**/*.{scss,css}'], ['styles', reload]);
   gulp.watch(['app/scripts/**/*.js'], ['jshint']);
   gulp.watch(['app/images/**/*'], reload);
 });
