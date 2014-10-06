3930816d047c5581ebdc8b0d2ba8db960502e252
Matt Gaunt <mattgaunt@mattgaunt-macbookpro.roam.corp.google.com>
2014-10-06 13:35:36 +0100

    First cut of focus styles for buttons

--- app/styleguide/button/button.js
+++ app/styleguide/button/button.js
@@ -21,19 +21,24 @@
     }
 
     function onClick(evt) {
-
+      console.log('onClick');
       if (frameCount > 0) {
         return;
       }
 
       frameCount = 1;
       bound = evt.currentTarget.getBoundingClientRect();
-      x = Math.round(evt.clientX - bound.left);
-      y = Math.round(evt.clientY - bound.top);
+      // Check if we are handling a keyboard click
+      if (event.clientX === 0 && event.clientY === 0) {
+        x = Math.round(bound.width / 2);
+        y = Math.round(bound.height / 2);
+      } else {
+        x = Math.round(evt.clientX - bound.left);
+        y = Math.round(evt.clientY - bound.top);
+      }
       transformString = 'translate(-50%, -50%) ' +
         'translate(' + x + 'px, ' + y + 'px) ' +
         'scale(0.0001, 0.0001)';
-
       ripple = evt.currentTarget.querySelector('.ripple');
 
       if (ripple !== null) {
