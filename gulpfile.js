63b1da91db137e6bd300116754163e4afa08315e
Addy Osmani <addyosmani@gmail.com>
2014-10-17 09:25:47 +0100

    Add back precision

--- gulpfile.js
+++ gulpfile.js
@@ -88,7 +88,9 @@ gulp.task('styles', function () {
     'app/styles/components/components.scss'
   ])
     .pipe($.changed('styles', {extension: '.scss'}))
-    .pipe($.sass())
+    .pipe($.sass({
+      precision: 10
+    }))
     .on('error', console.error.bind(console))
     .pipe($.autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
     .pipe(gulp.dest('.tmp/styles'))
