6b5fb2b644d58ccf8b3a1206f35ea6305b750adf
Addy Osmani <addyosmani@gmail.com>
2014-11-07 16:19:52 +0000

    Add initial tooltip implementation

---
+++ app/styleguide/tooltip/tooltip.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  var tooltips = Array.prototype.slice.call(document.querySelectorAll('.tooltip'));
+
+  initializeCheckboxes = function(els) {
+
+    if (!!els) {
+      Array.prototype.forEach.call(els, function(el, i) {
+        var forElId = el.getAttribute('for');
+        var forEl = document.querySelector('#' + forElId);
+        forEl.addEventListener('mouseenter', function(e) {
+          e.stopPropagation();
+          var element = document.querySelector('.tooltip[for="' + this.id + '"]');
+          var props = this.getBoundingClientRect();
+          element.style.left = props.left + (props.width / 2) + 'px';
+          element.style.marginLeft = -1 * (element.offsetWidth / 2) + 'px';
+          element.style.top = props.top + props.height + 10 + 'px';
+          element.classList.add('active');
+        }, false);
+        forEl.addEventListener('mouseleave', function(e) {
+          e.stopPropagation();
+          var element = document.querySelector('.tooltip[for="' + this.id + '"]');
+          element.classList.remove('active');
+        });
+      });
+    }
+  }
+  initializeCheckboxes(tooltips);
+});
