0dad80b350a3d9f6ba1c7c186887d23e36234651
Matt Gaunt <matt@gauntface.co.uk>
2014-12-16 14:01:20 +0000

    bem'ing up the item

--- app/styleguide/item/item.js
+++ app/styleguide/item/item.js
@@ -2,13 +2,16 @@
 
 window.addEventListener('load', function() {
   var itemElementsWithRipples =
-      document.querySelectorAll('.PaperItem.wsk-js-ripple-effect');
+      document.querySelectorAll('.wsk-js-ripple-effect');
+  
   for (var i = 0; i < itemElementsWithRipples.length; i++) {
     var rippleContainer = document.createElement('span');
-    rippleContainer.classList.add('PaperItem-rippleContainer');
+    rippleContainer.classList.add('wsk-item--ripple-container');
+    
     var ripple = document.createElement('span');
     ripple.classList.add('wsk-ripple');
     rippleContainer.appendChild(ripple);
+    
     itemElementsWithRipples[i].appendChild(rippleContainer);
   }
 });
