8420eab629876ce619afeb18b1d98d7aabee3d63
Sérgio Gomes <sgomes@google.com>
2014-12-17 16:11:41 +0000

    Some tooltip cleanups: - Applying new CSS naming convention - Cleaning up the JS somewhat

--- app/styleguide/tooltip/tooltip.js
+++ app/styleguide/tooltip/tooltip.js
@@ -2,30 +2,28 @@
 
 window.addEventListener('load', function() {
 
-  var tooltips = Array.prototype.slice.call(document.querySelectorAll('.tooltip'));
+  var tooltips = document.querySelectorAll('.wsk-tooltip');
 
   function initializeCheckboxes(els) {
-
     if (!!els) {
       Array.prototype.forEach.call(els, function(el, i) {
         var forElId = el.getAttribute('for');
         var forEl = document.querySelector('#' + forElId);
         forEl.addEventListener('mouseenter', function(e) {
           e.stopPropagation();
-          var element = document.querySelector('.tooltip[for="' + this.id + '"]');
           var props = this.getBoundingClientRect();
-          element.style.left = props.left + (props.width / 2) + 'px';
-          element.style.marginLeft = -1 * (element.offsetWidth / 2) + 'px';
-          element.style.top = props.top + props.height + 10 + 'px';
-          element.classList.add('active');
+          el.style.left = props.left + (props.width / 2) + 'px';
+          el.style.marginLeft = -1 * (el.offsetWidth / 2) + 'px';
+          el.style.top = props.top + props.height + 10 + 'px';
+          el.classList.add('is-active');
         }, false);
         forEl.addEventListener('mouseleave', function(e) {
           e.stopPropagation();
-          var element = document.querySelector('.tooltip[for="' + this.id + '"]');
-          element.classList.remove('active');
+          el.classList.remove('is-active');
         });
       });
     }
   }
+
   initializeCheckboxes(tooltips);
 });
