428701e1efc36c529e44a8dc718f3ed4e34efcc9
Jason Mayes <hello@jasonmayes.com>
2014-12-21 14:41:04 +0000

    Converted Tooltip to new JS design pattern
    Converted Tooltip component to use the new JS Design pattern as defined
    at http://codepen.io/jasonmayes/pen/146b4483d870f79f4d33254311703ac2

--- app/styleguide/tooltip/tooltip.js
+++ app/styleguide/tooltip/tooltip.js
@@ -1,29 +1,101 @@
-'use strict';
+/**
+ * Class constructor for Tooltip WSK component.
+ * Implements WSK component design pattern defined at:
+ * http://codepen.io/jasonmayes/pen/146b4483d870f79f4d33254311703ac2
+ * @param {HTMLElement} element The element that will be upgraded.
+ */
+function MaterialTooltip(element) {
+  'use strict';
+
+  this.element_ = element;
+
+  // Initialize instance.
+  this.init();
+}
+
+/**
+ * Store constants in one place so they can be updated easily.
+ * @enum {string | number}
+ * @private
+ */
+MaterialTooltip.prototype.Constant_ = {
+  // None for now.
+};
+
+/**
+ * Store strings for class names defined by this component that are used in
+ * JavaScript. This allows us to simply change it in one place should we
+ * decide to modify at a later date.
+ * @enum {string}
+ * @private
+ */
+MaterialTooltip.prototype.CssClasses_ = {
+  /**
+   * Class names should use camelCase and be prefixed with the word "material"
+   * to minimize conflict with 3rd party systems.
+   */
+
+  // TODO: Upgrade classnames in HTML / CSS / JS to use material prefix to
+  // reduce conflict and convert to camelCase for consistency.
+  IS_ACTIVE: 'is-active'
+};
+
+
+/**
+ * Handle mouseenter for tooltip.
+ * @param {Event} event The event that fired.
+ * @private
+ */
+MaterialTooltip.prototype.handleMouseEnter_ = function(event) {
+  'use strict';
+
+  event.stopPropagation();
+  var props = event.target.getBoundingClientRect();
+  this.element_.style.left = props.left + (props.width / 2) + 'px';
+  this.element_.style.marginLeft = -1 * (this.element_.offsetWidth / 2) + 'px';
+  this.element_.style.top = props.top + props.height + 10 + 'px';
+  this.element_.classList.add(this.CssClasses_.IS_ACTIVE);
+};
+
+
+/**
+ * Handle mouseleave for tooltip.
+ * @param {Event} event The event that fired.
+ * @private
+ */
+MaterialTooltip.prototype.handleMouseLeave_ = function(event) {
+  'use strict';
+
+  event.stopPropagation();
+  this.element_.classList.remove(this.CssClasses_.IS_ACTIVE);
+};
+
+
+/**
+ * Initialize element.
+ */
+MaterialTooltip.prototype.init = function() {
+  'use strict';
+
+  if (this.element_) {
+    var forElId = this.element_.getAttribute('for');
+    var forEl = document.getElementById(forElId);
+
+    forEl.addEventListener('mouseenter', this.handleMouseEnter_.bind(this),
+        false);
+    forEl.addEventListener('mouseleave', this.handleMouseLeave_.bind(this));
+  }
+};
+
 
 window.addEventListener('load', function() {
+  'use strict';
 
-  var tooltips = document.querySelectorAll('.wsk-tooltip');
-
-  function initializeCheckboxes(els) {
-    if (!!els) {
-      Array.prototype.forEach.call(els, function(el, i) {
-        var forElId = el.getAttribute('for');
-        var forEl = document.querySelector('#' + forElId);
-        forEl.addEventListener('mouseenter', function(e) {
-          e.stopPropagation();
-          var props = this.getBoundingClientRect();
-          el.style.left = props.left + (props.width / 2) + 'px';
-          el.style.marginLeft = -1 * (el.offsetWidth / 2) + 'px';
-          el.style.top = props.top + props.height + 10 + 'px';
-          el.classList.add('is-active');
-        }, false);
-        forEl.addEventListener('mouseleave', function(e) {
-          e.stopPropagation();
-          el.classList.remove('is-active');
-        });
-      });
-    }
-  }
-
-  initializeCheckboxes(tooltips);
+  // On document ready, the component registers itself. It can assume
+  // componentHandler is available in the global scope.
+  componentHandler.register({
+    constructor: MaterialTooltip,
+    classAsString: 'MaterialTooltip',
+    cssClass: 'wsk-tooltip'
+  });
 });
