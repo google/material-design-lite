a395041cc8e8e67aa48e59ac983dfbf8e37245c1
Addy Osmani <addyosmani@gmail.com>
2015-01-06 13:42:25 +0000

    Gulpfile: Drop unneeded line

--- gulpfile.js
+++ gulpfile.js
@@ -143,8 +143,6 @@ gulp.task('html', function() {
     .pipe($.if('*.css', $.csso()))
     .pipe(assets.restore())
     .pipe($.useref())
-    // Update Production Style Guide Paths
-    .pipe($.replace('components/components.css', 'components/main.min.css'))
     // Minify Any HTML
     .pipe($.if('*.html', $.minifyHtml()))
     // Output Files
