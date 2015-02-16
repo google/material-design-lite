79f5d75d0c48af16cc5666d9d9987a6002c7815e
Addy Osmani <addyosmani@gmail.com>
2015-02-16 14:06:43 +0000

    Styleguide => Lite

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 /**
  *
- *  Material Design Styleguide
+ *  Material Design Lite
  *  Copyright 2015 Google Inc. All rights reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
