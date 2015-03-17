666e6600249058583fa97fc952a98f5cefc94916
Alexander Surma <surma@surmair.de>
2015-03-17 14:56:28 +0000

    Write unit test for MaterialLayout

---
+++ test/unit/layout.js
@@ -0,0 +1,21 @@
+
+  describe('layout tests', function () {
+
+    it('Should have MaterialLayout globally available', function () {
+      expect(MaterialLayout).to.be.a('function');
+    });
+
+    it('Should be upgraded to a MaterialLayout successfully', function () {
+      var el = document.createElement('div');
+      el.innerHTML = '<div class="wsk-layout__header"></div>' +
+      '<div class="wsk-layout__drawer"></div>' +
+      '<div class="wsk-layout__content"></div>';
+
+      var parent = document.createElement('div');
+      parent.appendChild(el); // MaterialLayout.init() expects a parent
+
+      componentHandler.upgradeElement(el, 'MaterialLayout');
+      var upgraded = el.getAttribute('data-upgraded');
+      expect(upgraded).to.contain('MaterialLayout');
+    });
+  });
