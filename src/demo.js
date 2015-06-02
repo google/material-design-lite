4e8e52ea1b1e26111ab6e219ec4be2c915bd5974
Sérgio Gomes <sgomes@google.com>
2015-06-02 14:59:21 +0100

    Squashing all of @marcacohen 's work on partial component pages so far.
    The commit history was a bit too complicated, which made it hard to rebase.

---
+++ src/demo.js
@@ -0,0 +1,36 @@
+'use strict';
+
+function addPenButtons() {
+  var codeList = document.getElementsByTagName('pre');
+  
+  for (var i = 0, len = codeList.length; i < len; i++) {
+  	var elem = codeList[i],
+  		HTML = '',
+        CSS = '',
+        JS = '';
+        
+    HTML = elem.innerText;
+    var data = {
+  	  html               : HTML,
+  	  css                : CSS,
+  	  js                 : JS
+	  };
+    var JSONstring = 
+      JSON.stringify(data)
+      // Quotes will screw up the JSON
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&apos;');
+
+    var form = 
+      '<form name="pen-form" action="http://codepen.io/pen/define" method="POST" target="_blank">' + 
+        '<input type="hidden" name="data" value=\'' + 
+        JSONstring +
+        '\'>' +
+        '<input type="image" src="http://s.cdpn.io/3/cp-arrow-right.svg" class="codepen-mover-button" width="40" height="40" value="Create New Pen with Prefilled Data">' +
+      '</form>';
+
+    elem.innerHTML += form;
+  }
+}
+
+addPenButtons();
