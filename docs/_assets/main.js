fdb794aca54e34e5773ae6ed6a7c06948d7cf67f
Alexander Surma <surma@surmair.de>
2015-06-22 12:08:09 +0100

    Show WIP banner in production

--- docs/_assets/main.js
+++ docs/_assets/main.js
@@ -1,15 +1,30 @@
 (function() {
+  'use strict';
+
   var rightScroll = document.querySelector('.scrollindicator.scrollindicator--right');
   var leftScroll = document.querySelector('.scrollindicator.scrollindicator--left');
   var menuBar = document.querySelector('.docs-navigation');
   var delta = 40;
 
   function scrollMenuBar(delta) {
-      menuBar.scrollLeft += delta;
+    menuBar.scrollLeft += delta;
   }
 
   rightScroll.addEventListener('click', scrollMenuBar.bind(null, delta));
   rightScroll.addEventListener('tap', scrollMenuBar.bind(null, delta));
   leftScroll.addEventListener('click', scrollMenuBar.bind(null, -delta));
   leftScroll.addEventListener('tap', scrollMenuBar.bind(null, -delta));
+})();
+
+(function() {
+  'use strict';
+
+  var banner = document.querySelector('.docs-wip-banner');
+  if ((location.hostname !== 'localhost') &&
+      (location.hostname !== 'storage.googleapis.com')) {
+    banner.style.display = 'block';
+  }
+  if (location.search.indexOf('showbanner') !== -1) {
+    banner.style.display = 'block';
+  }
 })();
