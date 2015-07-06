7f81766ba414377235f75bbf20b315afa31fe929
Addy Osmani <addyosmani@gmail.com>
2015-07-06 13:26:11 +0100

    Adding dist

---
+++ assets/index.js
@@ -0,0 +1,1 @@
+!function(){Array.prototype.forEach.call(document.querySelectorAll("[data-target]"),function(t){var e=t.getAttribute("data-target");t.addEventListener("click",function(){location.href=e})})}();
