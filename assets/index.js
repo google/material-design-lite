ea8f4d9c9a04aa539b76613817dce7534767dadf
Addy Osmani <addyosmani@gmail.com>
2015-07-06 13:29:52 +0100

    Updating to latest stable release

--- assets/index.js
+++
@@ -1,1 +0,0 @@
-!function(){Array.prototype.forEach.call(document.querySelectorAll("[data-target]"),function(t){var e=t.getAttribute("data-target");t.addEventListener("click",function(){location.href=e})})}();
