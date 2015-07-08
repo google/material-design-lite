31d82013c411b65a4dabbd495b92d86ad0e0456b
Greg Thornton <xdissent@me.com>
2015-07-08 11:26:28 -0500

    Upgrade element based on css class names

--- src/mdlComponentHandler.js
+++ src/mdlComponentHandler.js
@@ -86,6 +86,15 @@ var componentHandler = (function() {
    * the element to.
    */
   function upgradeElementInternal(element, jsClass) {
+    if (jsClass === undefined) {
+      for (var i = 0; i < registeredComponents_.length; i++) {
+        if (element.classList.contains(registeredComponents_[i].cssClass)) {
+          upgradeElementInternal(element, registeredComponents_[i].className);
+        }
+      }
+      return;
+    }
+
     // Only upgrade elements that have not already been upgraded.
     var dataUpgraded = element.getAttribute('data-upgraded');
 
