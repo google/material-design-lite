ad55ba59d7ad9bb2e3eb6adf8e9763943c43af81
Xingchen Hong <hello@xc-h.net>
2015-07-08 14:19:22 -0500

    Reformatted to fit .jscsrc config.

--- src/third_party/Meteor.js
+++ src/third_party/Meteor.js
@@ -1,12 +1,11 @@
 // Export componentHandler in Meteor.
-
 (function() {
 'use strict';
-	if (typeof Meteor === 'object' && Meteor.isClient === true) {
-		// When in a Meteor package, componentHandler is not global.
-		// Attach it to window object.
-		window.componentHandler = componentHandler;
-		// Attach to Meteor object?
-// 		Meteor.mdlComponentHandler = componentHandler;
-	}
+  if (typeof Meteor === 'object' && Meteor.isClient === true) {
+    // When in a Meteor package, componentHandler is not global.
+    // Attach it to window object.
+    window.componentHandler = componentHandler;
+    // Attach to Meteor object?
+    //Meteor.mdlComponentHandler = componentHandler;
+  }
 })();
