b91dfd23ce96433bda8e2f8dff5ab5c209c0ed09
Ruslan Kyba <kybargr@gmail.com>
2015-07-19 00:19:52 +0300

    Tooltip positions
    Tooltip position according to class modifier (default BOTTOM)
    BOTTOM: 'mdl-tooltip--bottom',
    LEFT: 'mdl-tooltip--left',
    RIGHT: 'mdl-tooltip--right',
    TOP: 'mdl-tooltip--top'

--- src/tooltip/tooltip.js
+++ src/tooltip/tooltip.js
@@ -15,6 +15,7 @@
  * limitations under the License.
  */
 
+<<<<<<< HEAD
 (function() {
   'use strict';
 
@@ -67,12 +68,123 @@
     var left = props.left + (props.width / 2);
     var marginLeft = -1 * (this.element_.offsetWidth / 2);
 
+=======
+/**
+ * Class constructor for Tooltip MDL component.
+ * Implements MDL component design pattern defined at:
+ * https://github.com/jasonmayes/mdl-component-design-pattern
+ * @param {HTMLElement} element The element that will be upgraded.
+ */
+function MaterialTooltip(element) {
+  'use strict';
+
+  this.element_ = element;
+
+  // Initialize instance.
+  this.init();
+}
+
+/**
+ * Store constants in one place so they can be updated easily.
+ * @enum {string | number}
+ * @private
+ */
+MaterialTooltip.prototype.Constant_ = {
+  // None for now.
+};
+
+/**
+ * Store strings for class names defined by this component that are used in
+ * JavaScript. This allows us to simply change it in one place should we
+ * decide to modify at a later date.
+ * @enum {string}
+ * @private
+ */
+MaterialTooltip.prototype.CssClasses_ = {
+  IS_ACTIVE: 'is-active',
+  BOTTOM: 'mdl-tooltip--bottom',
+  LEFT: 'mdl-tooltip--left',
+  RIGHT: 'mdl-tooltip--right',
+  TOP: 'mdl-tooltip--top'
+};
+
+/**
+ * Handle mouseenter for tooltip.
+ * @param {Event} event The event that fired.
+ * @private
+ */
+MaterialTooltip.prototype.handleMouseEnter_ = function(event) {
+  'use strict';
+
+  event.stopPropagation();
+  var props = event.target.getBoundingClientRect();
+  var left = props.left + (props.width / 2);
+  var top = props.top + (props.height / 2);
+  var marginLeft = -1 * (this.element_.offsetWidth / 2);
+  var marginTop = -1 * (this.element_.offsetHeight / 2);
+
+  if(this.element_.classList.contains(this.CssClasses_.LEFT) || this.element_.classList.contains(this.CssClasses_.RIGHT)) {
+    if (top + marginTop < 0) {
+      this.element_.style.top = 0;
+      this.element_.style.marginTop = 0;
+    } else {
+      this.element_.style.top = top + 'px';
+      this.element_.style.marginTop = marginTop + 'px';
+    }
+  } else {
+>>>>>>> Tooltip positions
     if (left + marginLeft < 0) {
       this.element_.style.left = 0;
       this.element_.style.marginLeft = 0;
     } else {
       this.element_.style.left = left + 'px';
       this.element_.style.marginLeft = marginLeft + 'px';
+<<<<<<< HEAD
+=======
+    }
+  }
+
+  if(this.element_.classList.contains(this.CssClasses_.TOP)) {
+    this.element_.style.top = props.top - this.element_.offsetHeight - 10 + 'px';
+  } else if(this.element_.classList.contains(this.CssClasses_.RIGHT)) {
+    this.element_.style.left = props.left + props.width + 10 + 'px';
+  } else if(this.element_.classList.contains(this.CssClasses_.LEFT)) {
+    this.element_.style.left = props.left - this.element_.offsetWidth - 10 + 'px';
+  } else {
+    this.element_.style.top = props.top + props.height + 10 + 'px';
+  }
+
+  this.element_.classList.add(this.CssClasses_.IS_ACTIVE);
+  window.addEventListener('scroll', this.boundMouseLeaveHandler, false);
+  window.addEventListener('touchmove', this.boundMouseLeaveHandler, false);
+};
+
+/**
+ * Handle mouseleave for tooltip.
+ * @param {Event} event The event that fired.
+ * @private
+ */
+MaterialTooltip.prototype.handleMouseLeave_ = function(event) {
+  'use strict';
+
+  event.stopPropagation();
+  this.element_.classList.remove(this.CssClasses_.IS_ACTIVE);
+  window.removeEventListener('scroll', this.boundMouseLeaveHandler);
+  window.removeEventListener('touchmove', this.boundMouseLeaveHandler, false);
+};
+
+/**
+ * Initialize element.
+ */
+MaterialTooltip.prototype.init = function() {
+  'use strict';
+
+  if (this.element_) {
+    var forElId = this.element_.getAttribute('for');
+
+    if (forElId) {
+      this.forElement_ = document.getElementById(forElId);
+>>>>>>> Tooltip positions
     }
 
     this.element_.style.top = props.top + props.height + 10 + 'px';
