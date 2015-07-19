384315ca649c52e84f60211ded04cf794d08cd5a
Xingchen Hong <hello@xc-h.net>
2015-07-19 15:41:07 -0500

    Fixed test case 'should upgrade a single component to an element by using its CSS classes' to reveal a bug in the current `upgradeElement` caused by `dataUpgraded` not being correctly updated. Added test case 'should upgrade a single component to an element' to reveal a bug in the current `upgradeElement` caused by unreliably checking of upgraded class.
    Added `getUpgradedListOfElement_(element)` for getting a list of names of the upgraded classes.
    Made `isElementUpgraded_(element, jsClass)` more reliable.
    In `upgradeElementInternal(element, optJsClass)`:
    * Added argument verification.
    * Improved class search efficiency when jsClass is omitted.
    * Made `dataUpgraded` update correctly. (Closes #1115)

--- test/unit/componentHandler.js
+++ test/unit/componentHandler.js
@@ -97,9 +97,9 @@ describe('componentHandler', function() {
 
   it('should upgrade a single component to an element by using its CSS classes', function() {
     var el = document.createElement('button');
-    el.className = 'mdl-button mdl-js-button';
+    el.className = 'mdl-js-button mdl-js-ripple-effect';
     componentHandler.upgradeElement(el);
-    expect($(el)).to.have.data('upgraded', ',MaterialButton');
+    expect($(el)).to.have.data('upgraded', ',MaterialButton,MaterialRipple');
   });
 
   it('should upgrade the entire DOM available', function() {
@@ -114,6 +114,14 @@ describe('componentHandler', function() {
     expect($(buttonTwo)).to.have.data('upgraded', ',MaterialButton,MaterialRipple');
     document.body.removeChild(button);
     document.body.removeChild(buttonTwo);
+  });
+
+  it('should upgrade a single component to an element', function() {
+    var el = document.createElement('button');
+    el.setAttribute('data-upgraded', ',MaterialButtonPostfix');
+    el.className = 'mdl-js-button';
+    componentHandler.upgradeElement(el);
+    expect($(el)).to.have.data('upgraded', ',MaterialButtonPostfix,MaterialButton');
   });
 
   it('should upgrade all elements and their children within an HTMLCollection', function() {
