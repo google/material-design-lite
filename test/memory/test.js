d5895f6fdc92c63910029728fde71ff07d468d3a
Sam Saccone <samccone@gmail.com>
2015-08-07 09:55:17 -0400

    Add menu memory retry logic
    xref #1327 (Closes #1329)

--- test/memory/test.js
+++ test/memory/test.js
@@ -5,28 +5,43 @@ var path = require('path');
 var humanize = require('humanize');
 var assert = require('assert');
 var webdriver = drool.webdriver;
+var controlFlow = webdriver.promise.controlFlow();
 var driver = drool.start({chromeOptions: 'no-sandbox'});
+var stamps = [];
 
-drool.flow({
-  setup: function() {
-    driver.get('file://' + path.join(__dirname, '../../dist/components/menu/demo.html'));
-  },
-  action: function() {
-    driver.findElement(webdriver.By.css('#demo-menu-lower-left')).click();
-    driver.sleep(1000);
-    driver.findElement(webdriver.By.css('#demo-menu-lower-left')).click();
-  },
-  beforeAssert: function() {
-    driver.sleep(1000);
-  },
-  assert: function(after, initial) {
-    console.log('Menu Component Toggle Test');
-    console.log('node delta: ' + (after.nodes - initial.nodes));
-    console.log('heap delta: ' + humanize.filesize(after.jsHeapSizeUsed - initial.jsHeapSizeUsed));
-    console.log('event listener delta: ' + (after.jsEventListeners - initial.jsEventListeners));
+function measureMenuFlow(stamps, i) {
+  drool.flow({
+    setup: function() {
+      driver.get('file://' + path.join(__dirname, '../../dist/components/menu/demo.html'));
+    },
+    action: function() {
+      driver.findElement(webdriver.By.css('#demo-menu-lower-left')).click();
+      driver.sleep(1000);
+      driver.findElement(webdriver.By.css('#demo-menu-lower-left')).click();
+    },
+    beforeAssert: function() {
+      driver.sleep(1000);
+    },
+    assert: function(after, initial) {
+      console.log('Menu Component Toggle Test .. run: ' + (i + 1));
+      console.log('node delta: ' + (after.nodes - initial.nodes));
+      console.log('heap delta: ' + humanize.filesize(after.jsHeapSizeUsed - initial.jsHeapSizeUsed));
+      console.log('event listener delta: ' + (after.jsEventListeners - initial.jsEventListeners));
 
-    assert.equal(true, after.jsEventListeners <= initial.jsEventListeners);
-  }
-}, driver);
+      stamps.push([after.jsEventListeners, initial.jsEventListeners]);
+    }
+  }, driver);
+}
+
+for (var i = 0; i < 5; ++i) {
+  measureMenuFlow(stamps, i);
+}
+
+controlFlow.execute(function() {
+  stamps.some(function(stamp) {
+    assert.equal(true, stamp[0] <= stamp[1]);
+    return true;
+  });
+});
 
 driver.quit();
