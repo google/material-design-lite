0770c7ed2d50be4e64e152cf776729164ccd95fa
Surma <surma@surmair.de>
2015-08-19 15:47:22 +0100

    Add missing files to release folder (fixes #1422)

--- gulpfile.js
+++ gulpfile.js
@@ -640,7 +640,8 @@ gulp.task('publish:staging', function() {
 
 gulp.task('_release', function() {
   return gulp.src(['dist/material?(.min)@(.js|.css)?(.map)', 'LICENSE',
-    'README.md', 'bower.json', 'package.json', './sr?/**/*', 'gulpfile.js'])
+    'README.md', 'bower.json', 'package.json', '.jscsrc', '.jshintrc',
+    './sr?/**/*', 'gulpfile.js', './util?/**/*'])
     .pipe(gulp.dest('_release'));
 });
 
