b478b00ddf4329c2a6a592dcfe6c6a0886ec04f4
Endre Szasz-Revai <aeternala@gmail.com>
2015-08-30 00:26:22 +0200

    Required input fields should not be marked invalid (red) automatically
    If an input field is required then it will automatically be marked as invalid. This should not happen on a page load, but once a user interacts with it. 
    
    See issue #1502

--- src/textfield/textfield.js
+++ src/textfield/textfield.js
@@ -132,7 +132,9 @@
     if (this.input_.validity.valid) {
       this.element_.classList.remove(this.CssClasses_.IS_INVALID);
     } else {
-      this.element_.classList.add(this.CssClasses_.IS_INVALID);
+      if (this.element_.getElementsByTagName('input')[0].value.length > 0) {
+        this.element_.classList.add(this.CssClasses_.IS_INVALID);
+      }
     }
   };
 
