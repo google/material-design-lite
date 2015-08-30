0a563129c2b99b12d08fb7fc119443a8fabe1485
Endre Szasz-Revai <aeternala@gmail.com>
2015-08-30 08:55:05 +0200

    Thank you @surma - sorry I rushed the push without looking too much into the code.

--- src/textfield/textfield.js
+++ src/textfield/textfield.js
@@ -132,7 +132,7 @@
     if (this.input_.validity.valid) {
       this.element_.classList.remove(this.CssClasses_.IS_INVALID);
     } else {
-      if (this.element_.getElementsByTagName('input')[0].value.length > 0) {
+      if (this.input_.value && this.input_.value.length > 0) {
         this.element_.classList.add(this.CssClasses_.IS_INVALID);
       }
     }
