5e51a5f5aaebb8693f7d5f22f6c89a89c0c8af0e
Sérgio Gomes <sgomes@google.com>
2016-03-08 14:11:36 +0000

    Switching to ESLint from JSHint + JSCS.
    ESLint is a single, actively-maintained linter, which is easy to
    configure and has good presets. Switching to it allowed me to
    identify and correct a number of existing code quality issues.
    
    A lot of the work in this PR will probably be overriden by the move
    to the v2 component model later, but it's best to do things one
    step at a time :)
    
    Some highlights:
    - Splits 'lint' task in two: one for sources, one for aux files.
    - Sets two types of linting failures: errors and warnings.
      The tasks only fail for errors.
    - Adjusts line lengths all over the place (sticking to 80 char).
    - Doesn't switch any of the source to ES6, since we don't have the
      plumbing in place for that yet.

--- src/tabs/tabs.js
+++ src/tabs/tabs.js
@@ -71,13 +71,15 @@
    * @private
    */
   MaterialTabs.prototype.initTabs_ = function() {
-    if (this.element_.classList.contains(this.CssClasses_.MDL_JS_RIPPLE_EFFECT)) {
+    if (this.element_.classList.contains(
+        this.CssClasses_.MDL_JS_RIPPLE_EFFECT)) {
       this.element_.classList.add(
         this.CssClasses_.MDL_JS_RIPPLE_EFFECT_IGNORE_EVENTS);
     }
 
     // Select element tabs, document panels
-    this.tabs_ = this.element_.querySelectorAll('.' + this.CssClasses_.TAB_CLASS);
+    this.tabs_ =
+        this.element_.querySelectorAll('.' + this.CssClasses_.TAB_CLASS);
     this.panels_ =
         this.element_.querySelectorAll('.' + this.CssClasses_.PANEL_CLASS);
 
@@ -129,7 +131,8 @@
    */
   function MaterialTab(tab, ctx) {
     if (tab) {
-      if (ctx.element_.classList.contains(ctx.CssClasses_.MDL_JS_RIPPLE_EFFECT)) {
+      if (ctx.element_.classList.contains(
+          ctx.CssClasses_.MDL_JS_RIPPLE_EFFECT)) {
         var rippleContainer = document.createElement('span');
         rippleContainer.classList.add(ctx.CssClasses_.MDL_RIPPLE_CONTAINER);
         rippleContainer.classList.add(ctx.CssClasses_.MDL_JS_RIPPLE_EFFECT);
@@ -148,7 +151,6 @@
         tab.classList.add(ctx.CssClasses_.ACTIVE_CLASS);
         panel.classList.add(ctx.CssClasses_.ACTIVE_CLASS);
       });
-
     }
   }
 
