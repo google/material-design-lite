313a35028e4d2eb9eb1ba051461c2b92746fd51e
Jonathan Garbee <jonathan@garbee.me>
2016-05-17 16:51:43 -0400

    Call CustomEvent constructor instead of Event.

--- src/mdlComponentHandler.js
+++ src/mdlComponentHandler.js
@@ -229,7 +229,7 @@ componentHandler = (function() {
 
       var ev;
       if ('CustomEvent' in window && typeof window.CustomEvent === 'function') {
-        ev = new Event('mdl-componentupgraded', {
+        ev = new CustomEvent('mdl-componentupgraded', {
           bubbles: true, cancelable: false
         });
       } else {
@@ -360,7 +360,7 @@ componentHandler = (function() {
 
       var ev;
       if ('CustomEvent' in window && typeof window.CustomEvent === 'function') {
-        ev = new Event('mdl-componentdowngraded', {
+        ev = new CustomEvent('mdl-componentdowngraded', {
           bubbles: true, cancelable: false
         });
       } else {
