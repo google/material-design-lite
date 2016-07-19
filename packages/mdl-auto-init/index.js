e886ee923bbdd8c137b6494059cde10d29eaa570
Travis Kaufman <traviskaufman@google.com>
2016-07-19 14:02:05 -0400

    chore(infrastructure): Set up testing
    * `npm test` lints files, runs karma, reports coverages, checks coverage thresholds
    * `npm run test:watch` runs karma in auto-watch mode, with source maps
       for both source and test files.
    * `npm run fix` attempts to fix both CSS and JS lint issues in parallel
    * `npm run lint` lints JS and CSS in parallel
    * [karma](https://karma-runner.github.io/1.0/index.html) is used for running tests.
    * [tape](https://github.com/substack/tape) is used as the actual test runner.
    * [bel](https://github.com/shama/bel) is used for easy DOM fixtures.
    * [testdouble](https://github.com/testdouble/testdouble.js) is used for mocking/doubles.
    * [isparta](https://github.com/douglasduteil/isparta) is used to instrument source files for coverage.
    * [istanbul](https://github.com/gotwarlost/istanbul) is used to check and report coverage.
    * [eslint-plugin-tape](https://github.com/atabel/eslint-plugin-tape) is used for tape-specific test linting.
    
    resolves #4465

--- packages/mdl-auto-init/index.js
+++ packages/mdl-auto-init/index.js
@@ -1,10 +1,12 @@
 const registry = Object.create(null);
 
+const CONSOLE_WARN = console.warn.bind(console);
+
 /**
  * Auto-initializes all mdl components on a page.
  */
-export default function mdlAutoInit() {
-  const nodes = document.querySelectorAll('[data-mdl-auto-init]');
+export default function mdlAutoInit(root = document, warn = CONSOLE_WARN) {
+  const nodes = root.querySelectorAll('[data-mdl-auto-init]');
   for (let i = 0, node; (node = nodes[i]); i++) {
     const ctorName = node.dataset.mdlAutoInit;
     if (!ctorName) {
@@ -18,7 +20,7 @@ export default function mdlAutoInit() {
     }
 
     if (node[ctorName]) {
-      console.warn(`(mdl-auto-init) Component already initialized for ${node}. Skipping...`);
+      warn(`(mdl-auto-init) Component already initialized for ${node}. Skipping...`);
       continue;
     }
 
@@ -33,14 +35,22 @@ export default function mdlAutoInit() {
   }
 }
 
-mdlAutoInit.register = function(componentName, Ctor) {
+mdlAutoInit.register = function(componentName, Ctor, warn = CONSOLE_WARN) {
   if (typeof Ctor !== 'function') {
     throw new Error(`(mdl-auto-init) Invalid Ctor value ${Ctor}. Expected function`);
   }
   if (registry[componentName]) {
-    console.warn(
+    warn(
       `(mdl-auto-init) Overriding registration for ${componentName} with ${Ctor}. ` +
       `Was: ${registry[componentName]}`);
   }
   registry[componentName] = Ctor;
+};
+
+mdlAutoInit.deregister = function(componentName) {
+  delete registry[componentName];
+};
+
+mdlAutoInit.deregisterAll = function() {
+  Object.keys(registry).forEach(this.deregister, this);
 };
