40b26a0b7909be131e6abe6443f74feb33e36b20
Jonathan Garbee <jonathan@garbee.me>
2016-08-07 09:28:34 -0400

    feat(toolbar): Initial Implementation

--- webpack.config.js
+++ webpack.config.js
@@ -81,6 +81,7 @@ module.exports = [{
     'mdl-checkbox': path.resolve('./packages/mdl-checkbox/mdl-checkbox.scss'),
     'mdl-fab': path.resolve('./packages/mdl-fab/mdl-fab.scss'),
     'mdl-ripple': path.resolve('./packages/mdl-ripple/mdl-ripple.scss'),
+    'mdl-toolbar': path.resolve('./packages/mdl-toolbar/mdl-toolbar.scss'),
     'mdl-typography': path.resolve('./packages/mdl-typography/mdl-typography.scss')
   },
   output: {
