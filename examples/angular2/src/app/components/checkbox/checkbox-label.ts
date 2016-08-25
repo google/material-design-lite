c7e6f635d7d387c6c512637312357caf3a04926d
Travis Kaufman <traviskaufman@users.noreply.github.com>
2016-08-25 10:41:42 -0400

    refactor(examples): Update angular2 example [ci skip] (#4693)
    * Port angular2 over to foundation/adapter model
    * Update checkbox to match changes
    * Update examples/ README
    
    Part of #4569
    [#126819269]

--- examples/angular2/src/app/components/checkbox/checkbox-label.ts
+++ examples/angular2/src/app/components/checkbox/checkbox-label.ts
@@ -17,9 +17,9 @@
 import {Directive, HostBinding, Input} from 'angular2/core';
 
 @Directive({
-  selector: '[md-checkbox-label]'
+  selector: '[mdl-checkbox-label]'
 })
 export class CheckboxLabelDirective {
-  @HostBinding('class') className: string = 'md-checkbox-label';
+  @HostBinding('class') className: string = 'mdl-checkbox-label';
   @Input() id: string;
 }
