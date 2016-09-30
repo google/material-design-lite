720deed673741eb611c013183c11ff5bcc7af15f
aJchemist <aJchemist@users.noreply.github.com>
2016-09-30 18:12:26 +0900

    Fixed MaterialRadio.prototype.onChange_
    Fixed Uncaught TypeError: Cannot read property 'updateClasses_' of undefined

--- src/radio/radio.js
+++ src/radio/radio.js
@@ -82,7 +82,9 @@
       var button = radios[i].querySelector('.' + this.CssClasses_.RADIO_BTN);
       // Different name == different group, so no point updating those.
       if (button.getAttribute('name') === this.btnElement_.getAttribute('name')) {
-        radios[i]['MaterialRadio'].updateClasses_();
+        if (typeof radios[i]['MaterialRadio'] !== 'undefined') {
+            radios[i]['MaterialRadio'].updateClasses_();
+        }
       }
     }
   };
