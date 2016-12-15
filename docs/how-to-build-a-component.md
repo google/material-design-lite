cf5ac7e9c3c4db04f9046e7d45f42ed16c63a8fc
Sérgio Gomes <sgomes@google.com>
2016-12-15 18:18:57 +0000

    Update README.md with MDC-Web info.

--- docs/how-to-build-a-component.md
+++
@@ -1,1 +0,0 @@
-_Coming Soon!_
--- examples/angular2/src/app/components/form-field/index.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-
-import { FormFieldComponent } from './form-field';
-
-@NgModule({
-  exports: [FormFieldComponent],
-  declarations: [FormFieldComponent]
-})
-export class FormFieldModule {}
