1eab2bfe4f8c2e94b7cbe49c0e49f9000ef25402
Andres Rios <rianby64@gmail.com>
2016-12-30 14:18:55 +0300

    Check if menu is already into a container

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -98,22 +98,16 @@
   MaterialMenu.prototype.init = function() {
     if (this.element_) {
       var parent = this.element_.parentElement;
-      var createContainer = true;
-      if (parent) {
-        if (parent.classList.contains(this.CssClasses_.CONTAINER)) {
-          createContainer = false;
-        }
-      }
-      // Create container for the menu.
       var container;
-      if (createContainer) {
+      if (parent.classList.contains(this.CssClasses_.CONTAINER)) {
+        container = parent;
+      } else {
+        // Create container for the menu.
         container = document.createElement('div');
         container.classList.add(this.CssClasses_.CONTAINER);
         this.element_.parentElement.insertBefore(container, this.element_);
         this.element_.parentElement.removeChild(this.element_);
         container.appendChild(this.element_);
-      } else {
-        container = parent;
       }
       this.container_ = container;
 
