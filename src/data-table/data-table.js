47518d514f84c7f2d235f18334dc2781b0d7ab2f
sebtiz13 <sebtiz13@gmail.com>
2017-04-02 14:53:06 +0200

    Update data-table.js
    Thanks for your comments.
    I hope that this solution seems to you better

--- src/data-table/data-table.js
+++ src/data-table/data-table.js
@@ -76,9 +76,8 @@
           row.classList.add(this.CssClasses_.IS_SELECTED);
         } else {
           row.classList.remove(this.CssClasses_.IS_SELECTED);
-          var thead_checkbox = this.element_.tHead.querySelector('.mdl-checkbox.is-checked');
-          if(thead_checkbox !== null){
-            thead_checkbox['MaterialCheckbox'].uncheck();
+          if(this.headerCheckbox['MaterialCheckbox'].inputElement_.checked){
+            this.headerCheckbox['MaterialCheckbox'].uncheck();
           }
         }
       }.bind(this);
@@ -150,8 +149,8 @@
 
       if (this.element_.classList.contains(this.CssClasses_.SELECTABLE)) {
         var th = document.createElement('th');
-        var headerCheckbox = this.createCheckbox_(null, rows);
-        th.appendChild(headerCheckbox);
+        this.headerCheckbox = this.createCheckbox_(null, rows);
+        th.appendChild(this.headerCheckbox);
         firstHeader.parentElement.insertBefore(th, firstHeader);
 
         for (var i = 0; i < rows.length; i++) {
