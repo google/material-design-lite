79bb4be01b06e48701afc25f71483bce5b4a0b82
podcastkeeper <podcastkeeper@gmail.com>
2017-04-17 09:28:27 -0600

    Stopped using href attr on tabs if it's undefined
    If a user is managing tabs themselves with click callbacks instead of href there would be an error that prints out from trying to get charAt(0) of undefined. This change checks to make sure href exists before getting charAt(0).
    Functionally if you're not watching the log window there should be no change.

--- src/tabs/tabs.js
+++ src/tabs/tabs.js
@@ -139,7 +139,7 @@
       }
 
       tab.addEventListener('click', function(e) {
-        if (tab.getAttribute('href').charAt(0) === '#') {
+        if (tab.href && tab.href.charAt(0) === '#') {
           e.preventDefault();
           var href = tab.href.split('#')[1];
           var panel = ctx.element_.querySelector('#' + href);
