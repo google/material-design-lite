eccadc9e6571a95b92a946fc67914818e570fa78
Rody Davis <rody.davis.jr@gmail.com>
2022-08-01 15:16:22 -0700

    adding buttons

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [
+    require("autoprefixer"),
+    require("postcss-import"),
+    require("postcss-class-apply/dist/index"),
+    require("postcss-nested"),
+  ],
+};
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: "postcss-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
