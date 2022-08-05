9fb883611611c84503902dd612b9c11618ea3e66
Rody Davis <rody.davis.jr@gmail.com>
2022-08-05 12:29:05 -0700

    adding sticker sheet

--- src/config.ts
+++ src/config.ts
@@ -30,201 +30,233 @@ export const KNOWN_LANGUAGES = {
 //   apiKey: 'XXXXXXXXXX',
 // }
 
-export const SIDEBAR = {
-  en: [
-    { text: "", header: true },
-    { text: "Overview", header: true },
-    { text: "Introduction", link: "material-design-lite/introduction" },
-    // { text: "Theming", link: "material-design-lite/theming" },
-    { text: "Typography", link: "material-design-lite/typography" },
-    { text: "Color", link: "material-design-lite/color" },
-    { text: "Icons", link: "material-design-lite/icons" },
-    { text: "Shape", link: "material-design-lite/shape" },
-    { text: "FAQ", link: "material-design-lite/faq" },
-    // { text: "Tokens", link: "material-design-lite/tokens" },
+export const SIDEBAR = [
+  { text: "", header: true },
+  {
+    text: "Overview",
+    header: true
+  },
+  {
+    text: "Introduction",
+    link: "material-design-lite/introduction",
+  },
+  // { text: "Theming", link: "material-design-lite/theming" ,},
+  {
+    text: "Typography",
+    link: "material-design-lite/typography",
+  },
+  {
+    text: "Color",
+    link: "material-design-lite/color",
+  },
+  {
+    text: "Icons",
+    link: "material-design-lite/icons",
+  },
+  {
+    text: "Shape",
+    link: "material-design-lite/shape",
+  },
+  {
+    text: "FAQ",
+    link: "material-design-lite/faq",
+  },
+  // { text: "Tokens", link: "material-design-lite/tokens" },
 
-    {
-      text: "Components",
-      header: true
-    },
-    {
-      text: "Autocomplete",
-      link: "material-design-lite/components/autocomplete",
-    },
-    {
-      text: "Badge", link: "material-design-lite/components/badge",
-      draft: true
-    },
-    {
-      text: "Banner", link: "material-design-lite/components/banner",
-      draft: true
-    },
-    {
-      text: "Button",
-      link: "material-design-lite/components/button",
-    },
-    {
-      text: "Bottom app bar",
-      link: "material-design-lite/components/bottom-app-bar",
-      draft: true
-    },
-    {
-      text: "Bottom sheet",
-      link: "material-design-lite/components/bottom-sheet",
-      draft: true
-    },
-    {
-      text: "Card",
-      link: "material-design-lite/components/card",
-    },
-    {
-      text: "Chip",
-      link: "material-design-lite/components/chip",
-    },
-    {
-      text: "Checkbox",
-      link: "material-design-lite/components/checkbox",
-      draft: true
-    },
-    {
-      text: "Data Table",
-      link: "material-design-lite/components/data-table",
-      draft: true
-    },
-    {
-      text: "Divider",
-      link: "material-design-lite/components/data-table",
-      draft: true
-    },
-    {
-      text: "Date picker",
-      link: "material-design-lite/components/divider",
-      draft: true
-    },
-    {
-      text: "Dialog",
-      link: "material-design-lite/components/dialog",
-      draft: true
-    },
-    {
-      text: "Expansion Panel",
-      link: "material-design-lite/components/expansion-panel",
-      draft: true
-    },
-    {
-      text: "Image list",
-      link: "material-design-lite/components/image-list",
-      draft: true
-    },
-    {
-      text: "Icon",
-      link: "material-design-lite/components/icon",
-    },
-    {
-      text: "Floating action button",
-      link: "material-design-lite/components/fab",
-    },
-    {
-      text: "Grid list",
-      link: "material-design-lite/components/grid-list",
-      draft: true
-    },
-    {
-      text: "List",
-      link: "material-design-lite/components/list",
-      draft: true
-    },
-    {
-      text: "Menu",
-      link: "material-design-lite/components/menu",
-      draft: true
-    },
-    {
-      text: "Progress indicator",
-      link: "material-design-lite/components/progress-indicator",
-      draft: true
-    },
-    {
-      text: "Navigation bar",
-      link: "material-design-lite/components/navigation-bar",
-      draft: true
-    },
-    {
-      text: "Navigation drawer",
-      link: "material-design-lite/components/navigation-drawer",
-      draft: true
-    },
-    {
-      text: "Navigation rail",
-      link: "material-design-lite/components/navigation-rail",
-      draft: true
-    },
-    {
-      text: "Radio button",
-      link: "material-design-lite/components/radio-button",
-      draft: true
-    },
-    {
-      text: "Ripple",
-      link: "material-design-lite/components/ripple",
-    },
-    {
-      text: "Slider",
-      link: "material-design-lite/components/slide",
-      draft: true
-    },
-    {
-      text: "Select",
-      link: "material-design-lite/components/select",
-      draft: true
-    },
-    {
-      text: "Segmented",
-      link: "material-design-lite/components/segmented",
-      draft: true
-    },
-    {
-      text: "Stepper",
-      link: "material-design-lite/components/stepper",
-      draft: true
-    },
-    {
-      text: "Sort header",
-      link: "material-design-lite/components/sort-header",
-      draft: true
-    },
-    {
-      text: "Snackbar",
-      link: "material-design-lite/components/snackbar",
-      draft: true
-    },
-    {
-      text: "Switch",
-      link: "material-design-lite/components/switch",
-      draft: true
-    },
-    {
-      text: "Text field",
-      link: "material-design-lite/components/text-field",
-    },
-    {
-      text: "Tree",
-      link: "material-design-lite/components/tree",
-      draft: true
-    },
-    {
-      text: "Tooltip",
-      link: "material-design-lite/components/tooltip",
-      draft: true
-    },
-    {
-      text: "Tab",
-      link: "material-design-lite/components/tab",
-      draft: true
-    },
-    {
-      text: "Top app bar",
-      link: "material-design-lite/components/top-app-bar",
-    },
-  ],
-};
+  {
+    text: "Demos",
+    header: true
+  },
+  {
+    text: "Sticker sheet",
+    link: "material-design-lite/demos/sticker-sheet",
+  },
+  // {
+  //   text: "Dashboard",
+  //   link: "material-design-lite/demos/dashboard",
+  // },
+
+  {
+    text: "Components",
+    header: true
+  },
+  {
+    text: "Autocomplete",
+    link: "material-design-lite/components/autocomplete",
+  },
+  {
+    text: "Badge", link: "material-design-lite/components/badge",
+    draft: true
+  },
+  {
+    text: "Banner", link: "material-design-lite/components/banner",
+    draft: true
+  },
+  {
+    text: "Button",
+    link: "material-design-lite/components/button",
+  },
+  {
+    text: "Bottom app bar",
+    link: "material-design-lite/components/bottom-app-bar",
+    draft: true
+  },
+  {
+    text: "Bottom sheet",
+    link: "material-design-lite/components/bottom-sheet",
+    draft: true
+  },
+  {
+    text: "Card",
+    link: "material-design-lite/components/card",
+  },
+  {
+    text: "Chip",
+    link: "material-design-lite/components/chip",
+  },
+  {
+    text: "Checkbox",
+    link: "material-design-lite/components/checkbox",
+    draft: true
+  },
+  {
+    text: "Data Table",
+    link: "material-design-lite/components/data-table",
+    draft: true
+  },
+  {
+    text: "Divider",
+    link: "material-design-lite/components/data-table",
+    draft: true
+  },
+  {
+    text: "Date picker",
+    link: "material-design-lite/components/divider",
+    draft: true
+  },
+  {
+    text: "Dialog",
+    link: "material-design-lite/components/dialog",
+    draft: true
+  },
+  {
+    text: "Expansion Panel",
+    link: "material-design-lite/components/expansion-panel",
+    draft: true
+  },
+  {
+    text: "Image list",
+    link: "material-design-lite/components/image-list",
+    draft: true
+  },
+  {
+    text: "Icon",
+    link: "material-design-lite/components/icon",
+  },
+  {
+    text: "Floating action button",
+    link: "material-design-lite/components/fab",
+  },
+  {
+    text: "Grid list",
+    link: "material-design-lite/components/grid-list",
+    draft: true
+  },
+  {
+    text: "List",
+    link: "material-design-lite/components/list",
+    draft: true
+  },
+  {
+    text: "Menu",
+    link: "material-design-lite/components/menu",
+    draft: true
+  },
+  {
+    text: "Progress indicator",
+    link: "material-design-lite/components/progress-indicator",
+    draft: true
+  },
+  {
+    text: "Navigation bar",
+    link: "material-design-lite/components/navigation-bar",
+    draft: true
+  },
+  {
+    text: "Navigation drawer",
+    link: "material-design-lite/components/navigation-drawer",
+    draft: true
+  },
+  {
+    text: "Navigation rail",
+    link: "material-design-lite/components/navigation-rail",
+    draft: true
+  },
+  {
+    text: "Radio button",
+    link: "material-design-lite/components/radio-button",
+    draft: true
+  },
+  {
+    text: "Ripple",
+    link: "material-design-lite/components/ripple",
+  },
+  {
+    text: "Slider",
+    link: "material-design-lite/components/slide",
+    draft: true
+  },
+  {
+    text: "Select",
+    link: "material-design-lite/components/select",
+    draft: true
+  },
+  {
+    text: "Segmented",
+    link: "material-design-lite/components/segmented",
+    draft: true
+  },
+  {
+    text: "Stepper",
+    link: "material-design-lite/components/stepper",
+    draft: true
+  },
+  {
+    text: "Sort header",
+    link: "material-design-lite/components/sort-header",
+    draft: true
+  },
+  {
+    text: "Snackbar",
+    link: "material-design-lite/components/snackbar",
+    draft: true
+  },
+  {
+    text: "Switch",
+    link: "material-design-lite/components/switch",
+    draft: true
+  },
+  {
+    text: "Text field",
+    link: "material-design-lite/components/text-field",
+  },
+  {
+    text: "Tree",
+    link: "material-design-lite/components/tree",
+    draft: true
+  },
+  {
+    text: "Tooltip",
+    link: "material-design-lite/components/tooltip",
+    draft: true
+  },
+  {
+    text: "Tab",
+    link: "material-design-lite/components/tab",
+    draft: true
+  },
+  {
+    text: "Top app bar",
+    link: "material-design-lite/components/top-app-bar",
+  },
+];
---
+++ src/pages/demos/sticker-sheet.md
@@ -0,0 +1,33 @@
+---
+layout: "../../layouts/MainLayout.astro"
+title: "Sticker Sheet"
+---
+
+## Buttons
+
+<div class="row gap">
+    <button class="button">Default</button>
+    <button class="button filled">Filled</button>
+    <button class="button filled-tonal">Filled Tonal</button>
+    <button class="button elevated">Elevated</button>
+    <button class="button outlined">Outlined</button>
+    <button class="button text">Text</button>
+</div>
+
+## Icon Buttons
+
+<div class="row gap">
+    <button class="icon"><i class="material-icons">add</i></button>
+    <button class="icon contained filled"><i class="material-icons">add</i></button>
+    <button class="icon contained filled-tonal"><i class="material-icons">add</i></button>
+    <button class="icon contained outlined"><i class="material-icons">add</i></button>
+</div>
+
+## Floating Action Buttons
+
+<div class="row gap">
+    <button class="button fab extended">Extended</button>
+    <button class="button fab small"><i class="material-icons">add</i></button>
+    <button class="button fab"><i class="material-icons">add</i></button>
+    <button class="button fab large"><i class="material-icons">add</i></button>
+</div>
