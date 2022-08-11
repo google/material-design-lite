297671cd18c65cdbaf5097e8aa877751e46b06c6
Rody Davis <rody.davis.jr@gmail.com>
2022-08-11 14:02:54 -0700

    adding js files

---
+++ public/warn.js
@@ -0,0 +1,8 @@
+const _customElementsDefine = window.customElements.define;
+window.customElements.define = (name, cl, conf) => {
+  if (!customElements.get(name)) {
+    _customElementsDefine.call(window.customElements, name, cl, conf);
+  } else {
+    console.warn(name, "has been defined twice");
+  }
+};
