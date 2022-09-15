121c28d05ae1c7db9acb9ebebd017dc408cba5cb
Rody Davis <rody.davis.jr@gmail.com>
2022-09-15 15:14:48 -0700

    moving sub module

--- third_party/material-web-transformer/src/main.ts
+++ third_party/material-web-transformer/src/main.ts
@@ -6,101 +6,101 @@ import * as fs from "fs";
 // Material Web Styles
 
 // Autocomplete
-import { styles as autocompleteFilled } from "@material/web/autocomplete/lib/filled-styles.css.js";
-import { styles as autocompleteList } from "@material/web/autocomplete/lib/autocompletelist/autocomplete-list-styles.css.js";
+import { styles as autocompleteFilled } from "../../material-web/autocomplete/lib/filled-styles.css.js";
+import { styles as autocompleteList } from "../../material-web/autocomplete/lib/autocompletelist/autocomplete-list-styles.css.js";
 
 // Badge
-import { styles as badge } from "@material/web/badge/lib/badge-styles.css.js";
+import { styles as badge } from "../../material-web/badge/lib/badge-styles.css.js";
 
 // Button
-import { styles as button } from "@material/web/button/lib/shared-styles.css.js";
-import { styles as elevatedButton } from "@material/web/button/lib/elevated-styles.css.js";
-import { styles as outlinedButton } from "@material/web/button/lib/outlined-styles.css.js";
-import { styles as filledButton } from "@material/web/button/lib/filled-styles.css.js";
-import { styles as tonalButton } from "@material/web/button/lib/tonal-styles.css.js";
-import { styles as textButton } from "@material/web/button/lib/text-styles.css.js";
+import { styles as button } from "../../material-web/button/lib/shared-styles.css.js";
+import { styles as elevatedButton } from "../../material-web/button/lib/elevated-styles.css.js";
+import { styles as outlinedButton } from "../../material-web/button/lib/outlined-styles.css.js";
+import { styles as filledButton } from "../../material-web/button/lib/filled-styles.css.js";
+import { styles as tonalButton } from "../../material-web/button/lib/tonal-styles.css.js";
+import { styles as textButton } from "../../material-web/button/lib/text-styles.css.js";
 
 // Checkbox
-import { styles as checkbox } from "@material/web/checkbox/lib/checkbox-styles.css.js";
+import { styles as checkbox } from "../../material-web/checkbox/lib/checkbox-styles.css.js";
 
 // Elevation
-import { styles as elevation } from "@material/web/elevation/lib/elevation-overlay-styles.css.js";
+import { styles as elevation } from "../../material-web/elevation/lib/elevation-overlay-styles.css.js";
 
 // Fab
-import { styles as fab } from "@material/web/fab/lib/fab-styles.css.js";
-import { styles as fabShared } from "@material/web/fab/lib/fab-shared-styles.css.js";
-import { styles as fabExtended } from "@material/web/fab/lib/fab-extended-styles.css.js";
+import { styles as fab } from "../../material-web/fab/lib/fab-styles.css.js";
+import { styles as fabShared } from "../../material-web/fab/lib/fab-shared-styles.css.js";
+import { styles as fabExtended } from "../../material-web/fab/lib/fab-extended-styles.css.js";
 
 // Field
-import { styles as field } from "@material/web/field/lib/shared-styles.css.js";
-import { styles as filledField } from "@material/web/field/lib/filled-styles.css.js";
-import { styles as outlinedField } from "@material/web/field/lib/outlined-styles.css.js";
+import { styles as field } from "../../material-web/field/lib/shared-styles.css.js";
+import { styles as filledField } from "../../material-web/field/lib/filled-styles.css.js";
+import { styles as outlinedField } from "../../material-web/field/lib/outlined-styles.css.js";
 
 // Focus
-import { styles as focus } from "@material/web/focus/lib/focus-ring-styles.css.js";
+import { styles as focus } from "../../material-web/focus/lib/focus-ring-styles.css.js";
 
 // Form Field
-import { styles as formField } from "@material/web/formfield/lib/formfield-styles.css.js";
+import { styles as formField } from "../../material-web/formfield/lib/formfield-styles.css.js";
 
 // Icon
-import { styles as icon } from "@material/web/icon/lib/icon-styles.css.js";
+import { styles as icon } from "../../material-web/icon/lib/icon-styles.css.js";
 
 // Icon Button
-import { styles as iconButton } from "@material/web/iconbutton/lib/icon-button-styles.css.js";
-import { styles as iconButtonFilled } from "@material/web/iconbutton/lib/filled-styles.css.js";
-import { styles as iconButtonFilledTonal } from "@material/web/iconbutton/lib/filled-tonal-styles.css.js";
-import { styles as iconButtonOutlined } from "@material/web/iconbutton/lib/outlined-styles.css.js";
-import { styles as iconButtonStandard } from "@material/web/iconbutton/lib/standard-styles.css.js";
+import { styles as iconButton } from "../../material-web/iconbutton/lib/icon-button-styles.css.js";
+import { styles as iconButtonFilled } from "../../material-web/iconbutton/lib/filled-styles.css.js";
+import { styles as iconButtonFilledTonal } from "../../material-web/iconbutton/lib/filled-tonal-styles.css.js";
+import { styles as iconButtonOutlined } from "../../material-web/iconbutton/lib/outlined-styles.css.js";
+import { styles as iconButtonStandard } from "../../material-web/iconbutton/lib/standard-styles.css.js";
 
 // List
-import { styles as list } from "@material/web/list/lib/list-styles.css.js";
-import { styles as listItem } from "@material/web/list/lib/listitem/list-item-styles.css.js";
-import { styles as listVideo } from "@material/web/list/lib/video/list-item-video-styles.css.js";
-import { styles as listImage } from "@material/web/list/lib/image/list-item-image-styles.css.js";
-import { styles as listIcon } from "@material/web/list/lib/icon/list-item-icon-styles.css.js";
-import { styles as listDivider } from "@material/web/list/lib/divider/list-divider-styles.css.js";
-import { styles as listAvatar } from "@material/web/list/lib/avatar/list-item-avatar-styles.css.js";
+import { styles as list } from "../../material-web/list/lib/list-styles.css.js";
+import { styles as listItem } from "../../material-web/list/lib/listitem/list-item-styles.css.js";
+import { styles as listVideo } from "../../material-web/list/lib/video/list-item-video-styles.css.js";
+import { styles as listImage } from "../../material-web/list/lib/image/list-item-image-styles.css.js";
+import { styles as listIcon } from "../../material-web/list/lib/icon/list-item-icon-styles.css.js";
+import { styles as listDivider } from "../../material-web/list/lib/divider/list-divider-styles.css.js";
+import { styles as listAvatar } from "../../material-web/list/lib/avatar/list-item-avatar-styles.css.js";
 
 // Menu
-import { styles as menu } from "@material/web/menu/lib/menu-styles.css.js";
-import { styles as menuButton } from "@material/web/menu/lib/menu-button-styles.css.js";
+import { styles as menu } from "../../material-web/menu/lib/menu-styles.css.js";
+import { styles as menuButton } from "../../material-web/menu/lib/menu-button-styles.css.js";
 
 // Menu Surface
-import { styles as menuSurface } from "@material/web/menusurface/lib/menu-surface-styles.css.js";
+import { styles as menuSurface } from "../../material-web/menusurface/lib/menu-surface-styles.css.js";
 
 // Navigation Bar
-import { styles as navigationBar } from "@material/web/navigationbar/lib/navigation-bar-styles.css.js";
+import { styles as navigationBar } from "../../material-web/navigationbar/lib/navigation-bar-styles.css.js";
 
 // Navigation Drawer
-import { styles as navigationDrawer } from "@material/web/navigationdrawer/lib/navigation-drawer-styles.css.js";
-import { styles as navigationDrawerModal } from "@material/web/navigationdrawer/lib/navigation-drawer-modal-styles.css.js";
+import { styles as navigationDrawer } from "../../material-web/navigationdrawer/lib/navigation-drawer-styles.css.js";
+import { styles as navigationDrawerModal } from "../../material-web/navigationdrawer/lib/navigation-drawer-modal-styles.css.js";
 
 // Navigation Tab
-import { styles as navigationTab } from "@material/web/navigationtab/lib/navigation-tab-styles.css.js";
+import { styles as navigationTab } from "../../material-web/navigationtab/lib/navigation-tab-styles.css.js";
 
 // Radio
-import { styles as radio } from "@material/web/radio/lib/radio-styles.css.js";
+import { styles as radio } from "../../material-web/radio/lib/radio-styles.css.js";
 
 // Ripple
-import { styles as ripple } from "@material/web/ripple/lib/ripple-styles.css.js";
+import { styles as ripple } from "../../material-web/ripple/lib/ripple-styles.css.js";
 
 // Segmented Button
-import { styles as segmentedButton } from "@material/web/segmentedbutton/lib/segmented-button-styles.css.js";
-import { styles as segmentedButtonOutlined } from "@material/web/segmentedbutton/lib/outlined-styles.css.js";
+import { styles as segmentedButton } from "../../material-web/segmentedbutton/lib/segmented-button-styles.css.js";
+import { styles as segmentedButtonOutlined } from "../../material-web/segmentedbutton/lib/outlined-styles.css.js";
 
 // Segmented Button Set
-import { styles as segmentedButtonSet } from "@material/web/segmentedbuttonset/lib/shared-styles.css.js";
-import { styles as segmentedButtonSetOutlined } from "@material/web/segmentedbuttonset/lib/outlined-styles.css.js";
+import { styles as segmentedButtonSet } from "../../material-web/segmentedbuttonset/lib/shared-styles.css.js";
+import { styles as segmentedButtonSetOutlined } from "../../material-web/segmentedbuttonset/lib/outlined-styles.css.js";
 
 // Switch
-import { styles as switchStyles } from "@material/web/switch/lib/switch-styles.css.js";
+import { styles as switchStyles } from "../../material-web/switch/lib/switch-styles.css.js";
 
 // Text Field
-import { styles as textField } from "@material/web/textfield/lib/shared-styles.css.js";
-import { styles as filledTextField } from "@material/web/textfield/lib/filled-styles.css.js";
-import { styles as filledForceStylesTextField } from "@material/web/textfield/lib/filled-forced-colors-styles.css.js";
-import { styles as outlinedTextField } from "@material/web/textfield/lib/outlined-styles.css.js";
-import { styles as outlinedForcedColorsTextField } from "@material/web/textfield/lib/outlined-forced-colors-styles.css.js";
+import { styles as textField } from "../../material-web/textfield/lib/shared-styles.css.js";
+import { styles as filledTextField } from "../../material-web/textfield/lib/filled-styles.css.js";
+import { styles as filledForceStylesTextField } from "../../material-web/textfield/lib/filled-forced-colors-styles.css.js";
+import { styles as outlinedTextField } from "../../material-web/textfield/lib/outlined-styles.css.js";
+import { styles as outlinedForcedColorsTextField } from "../../material-web/textfield/lib/outlined-forced-colors-styles.css.js";
 
 const material = [
     {
