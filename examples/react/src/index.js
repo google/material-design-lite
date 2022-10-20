022496a6bdbf71f400d5d8bfd26b42e570fdc20a
Rody Davis <rody.davis.jr@gmail.com>
2022-10-20 12:03:20 -0700

    updating examples to use npm package

--- examples/react/src/index.js
+++ examples/react/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import "material-design-lite/css/core.css";
+import "material-design-lite/css/tokens/themes/auto.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
